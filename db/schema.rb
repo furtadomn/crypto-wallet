16c02b3b7d4e0768016c5f33cf83fa7ae51445e6
furtadomn <mnfurtado@yahoo.com.br>
2022-03-15 09:32:40 -0300

    delete and create MiningTypes CRUD again to fix description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_103024) do
+ActiveRecord::Schema.define(version: 2022_03_15_122905) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_103024) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
