7ff58c04499aeb38ebb4d43747dd6b5d81d47fa4
furtadomn <mnfurtado@yahoo.com.br>
2022-03-21 10:21:15 -0300

    create set_locale method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    locale = params[:locale] || I18n.default_locale
+    if locale
+      cookies[:locale] = locale
+    end
+
+    if cookies[:locale]
+      if I18n.default_locale != cookies[:locale]
+        I18n.default_locale = cookies[:locale]
+      end
+    end
+  end
 end
